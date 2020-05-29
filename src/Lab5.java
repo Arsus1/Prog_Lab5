a34e0757aafab73fae674c2d2ac8f4caa4a7d702
Arsus1 <kuzmin.ars@yandex.ru>
2020-05-29 16:04:53 +0300

    property fix

--- src/Lab5.java
+++ src/Lab5.java
@@ -1,4 +1,6 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Map;
 
 /**
  * Основной класс Лабораторной работы №5.
@@ -7,10 +9,14 @@ import java.io.File;
  */
 public class Lab5 {
     public static void main(String[] args) {
-        String fname = System.getenv("input");
-        File f = new File(fname);
-        CollectionWorker worker = new CollectionWorker(f);
-        worker.processCommands(System.in);
+        String fname = System.getProperty("input");
+        try {
+            File f = new File(fname);
+            CollectionWorker worker = new CollectionWorker(f);
+            worker.processCommands(System.in);
+        } catch (NullPointerException e) {
+            System.out.println("Некорректное имя файла!");
+        }
         // Обработка команд, передающихся из потока ввода
     }
 }
