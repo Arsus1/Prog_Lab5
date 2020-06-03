0631169978ec1edafa2a20c54941f3bb0581acdb
Arsus1 <kuzmin.ars@yandex.ru>
2020-06-03 21:11:05 +0300

    Helios fix

--- src/Lab5.java
+++ src/Lab5.java
@@ -9,14 +9,15 @@ import java.util.Map;
  */
 public class Lab5 {
     public static void main(String[] args) {
-        String fname = System.getProperty("input");
+        String fname = System.getenv("INPUT");
         try {
             File f = new File(fname);
             CollectionWorker worker = new CollectionWorker(f);
             worker.processCommands(System.in);
         } catch (NullPointerException e) {
-            System.out.println("Некорректное имя файла!");
+            System.out.println("Файл не найден!");
         }
+
         // Обработка команд, передающихся из потока ввода
     }
 }
