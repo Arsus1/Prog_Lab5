46fa95000b07e3a68a3a9843c930697dd5f79ab1
Arsus1 <kuzmin.ars@yandex.ru>
2020-06-10 02:07:17 +0300

    Big Fix java.io.InputStreamReader

--- src/Lab5.java
+++ src/Lab5.java
@@ -1,8 +1,6 @@
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Map;
 
 /**
  * Основной класс Лабораторной работы №5.
@@ -11,9 +9,7 @@ import java.util.Map;
  */
 public class Lab5 {
     public static void main(String[] args) {
-        //System.setProperty("INPUT", "test.xml");
-        //String fname = System.getenv("INPUT");
-        String fname = "test.xml";
+        String fname = System.getenv("INPUT");
         try {
             File f = new File(fname);
             CollectionWorker worker = new CollectionWorker(f);
